59768cc31b29faee6adcfad03c5db61c095e1e5f
18wh1a1220 <69300506+18wh1a1220@users.noreply.github.com>
2021-07-13 15:43:38 +0530

    Add files via upload

---
+++ Experiment 4/SendSMS.java
@@ -0,0 +1,98 @@
+package com.example.app4;
+
+import android.os.Bundle;
+import android.os.Build;
+import android.widget.Button;
+import android.widget.EditText;
+import android.util.Log;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.telephony.SmsManager;
+import android.text.TextUtils;
+import android.widget.Toast;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.example.myapplication.R;
+
+public class SendSMS extends AppCompatActivity {
+
+    private static final int PERMISSION_REQUEST_CODE = 1;
+    private Button sendSMS;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sendsms);
+
+        final EditText phoneNumber = (EditText) findViewById(R.id.phoneNumber);
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkPermission()) {
+                Log.e("permission", "Permission already granted.");
+            } else {
+                requestPermission();
+            }
+        }
+
+        final EditText smsText = (EditText) findViewById(R.id.message);
+        sendSMS = (Button) findViewById(R.id.sendSMS);
+        sendSMS.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String sms = smsText.getText().toString();
+                String phoneNum = phoneNumber.getText().toString();
+                if(!TextUtils.isEmpty(sms) && !TextUtils.isEmpty(phoneNum)) {
+                    if(checkPermission()) {
+
+//Get the default SmsManager//
+
+                        SmsManager smsManager = SmsManager.getDefault();
+
+//Send the SMS//
+
+                        smsManager.sendTextMessage(phoneNum, null, sms, null, null);
+                    }else {
+                        Toast.makeText(SendSMS.this, "Permission denied", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+
+    private boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(SendSMS.this, Manifest.permission.SEND_SMS);
+        if (result == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, PERMISSION_REQUEST_CODE);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Toast.makeText(SendSMS.this, "Permission accepted", Toast.LENGTH_LONG).show();
+
+                } else {
+                    Toast.makeText(SendSMS.this, "Permission denied", Toast.LENGTH_LONG).show();
+                    Button sendSMS = (Button) findViewById(R.id.sendSMS);
+                    sendSMS.setEnabled(false);
+
+                }
+                break;
+        }
+    }
+}
